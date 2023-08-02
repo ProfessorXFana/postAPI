f83737c12a1fb4d2f0c883d796e69fa0703b9452
Professor <sajal.voice@gmail.com>
2023-08-02 18:45:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+const submit = document.getElementById('submit').addEventListener('click', function(){
+    
+    const title = document.getElementById('title').value;
+    const bodyContent = document.getElementById('body-content').value;
+    const post = {title:title, body:bodyContent};
+    postToNewServer(post);
+})
+
+
+
+
+
+
+
+
+function postToNewServer(postInfo){
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        body:JSON.stringify(postInfo),
+        headers:{
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+}
+
+
+
+
